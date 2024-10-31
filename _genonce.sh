2dff2ea02fdb0749118594efe5a81ac458dfc934
daniel-thomson <mail@danielthomson.co.nz>
2024-11-01 11:57:02 +1300

    mem

--- _genonce.sh
+++ _genonce.sh
@@ -18,12 +18,12 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -Xms256M -Xmx8192M -jar $publisher -ig . $txoption $*
+	java -Xms256M -Xmx19008M -jar $publisher -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -Xms256M -Xmx8192M -jar $publisher -ig . $txoption $*
+		java -Xms256M -Xmx19008M -jar $publisher -ig . $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
